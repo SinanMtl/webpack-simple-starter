945a7cfb50e45618df1415999ac144b794f15221
Sinan Mutlu <sinanm17@gmail.com>
2017-11-20 00:50:03 +0300

    Object.observe and lodash removed

--- src/scripts/index.js
+++
@@ -1,7 +0,0 @@
-// console.log('Hello my friend. How are you?')
-import 'object.observe'
-
-window.object = {foo: null}
-Object.observe(window.object, (changes) => {
-  console.log('Changed:', changes)
-})
