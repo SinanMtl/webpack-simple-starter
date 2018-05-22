6d532b62603bebebcc2fdc211e66ba8e85978cb9
Aaron Zhou <aaaron.chou@gmail.com>
2018-05-22 21:47:37 +1200

    Fix autoprefixer

--- build/utils.js
+++ build/utils.js
@@ -24,12 +24,25 @@ exports.cssLoaders = function (options) {
   function generateLoaders (loader, loaderOptions) {
     var loaders = [cssLoader]
     if (loader) {
-      loaders.push({
-        loader: loader + '-loader',
-        options: Object.assign({}, loaderOptions, {
-          sourceMap: options.sourceMap
-        })
-      })
+      loaders.push(
+        {
+          loader: 'postcss-loader',
+          options: {
+              sourceMap: options.sourceMap,
+              plugins: function() {
+                  return [
+                      require('autoprefixer')
+                  ]
+              }
+          }
+        }, 
+        {
+          loader: loader + '-loader',
+          options: Object.assign({}, loaderOptions, {
+            sourceMap: options.sourceMap
+          })
+        }
+      )
     }
 
     return ExtractTextPlugin.extract({
@@ -40,7 +53,6 @@ exports.cssLoaders = function (options) {
 
   return {
     css: generateLoaders(),
-    postcss: generateLoaders(),
     less: generateLoaders('less'),
     sass: generateLoaders('sass', { indentedSyntax: true }),
     scss: generateLoaders('sass'),
